b31d82fc8b66897bfa7826a916673fb42c37b7db
Koki Ibukuro <koki.ibukuro@gmail.com>
2018-04-10 18:18:52 +0900

    First commit

---
+++ Assets/Script/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    [SerializeField]
+    Vector3 speed;
+
+    void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime);
+    }
+}
